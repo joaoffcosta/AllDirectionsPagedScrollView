a01c7f3742bf2b96df5700c704fc35fa946dcb1c
João Costa <joaostacosta@gmail.com>
2013-08-03 21:07:17 +0100

    Initial iOS project commit

---
+++ AllDirectionsPagedScrollView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AllDirectionsPagedScrollView
+//
+//  Created by Joao Costa on 8/3/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AllDirectionsPagedScrollView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AllDirectionsPagedScrollView
+//
+//  Created by Joao Costa on 8/3/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
