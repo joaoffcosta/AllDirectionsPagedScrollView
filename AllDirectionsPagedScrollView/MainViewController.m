7cb4104dccb2559968627b1e2c4ff7a4d62dbf3d
João Costa <joaostacosta@gmail.com>
2013-08-18 12:43:51 +0100

    Determines scroll direction based on first scroll offset

--- AllDirectionsPagedScrollView/MainViewController.m
+++ AllDirectionsPagedScrollView/MainViewController.m
@@ -3,9 +3,15 @@
 #define HOR_PAGES 5
 #define VER_PAGES 5
 
+enum ScrollDirection {
+    ScrollUp, ScrollRight, ScrollDown, ScrollLeft
+};
+
 @interface MainViewController ()
 
 @property(assign) CGPoint initialOffset;
+@property(assign) enum ScrollDirection scrollDirection;
+@property(assign) BOOL waitingFirstScroll;
 
 @end
 
@@ -43,6 +49,8 @@
             [scrollView addSubview:view];
         }
     }
+    
+    self.waitingFirstScroll = NO;
 }
 
 #pragma mark UIScrollViewDelegate
@@ -50,7 +58,38 @@
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
 {
     self.initialOffset = scrollView.contentOffset;
+    self.waitingFirstScroll = YES;
     NSLog(@"Offset before drag starts: (%f,%f)", scrollView.contentOffset.x, scrollView.contentOffset.y);
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    if (self.waitingFirstScroll) {
+        self.waitingFirstScroll = NO;
+        self.scrollDirection = [self scrollDirectionForOffset:scrollView.contentOffset];
+    }
+}
+
+- (enum ScrollDirection)scrollDirectionForOffset:(CGPoint)offset
+{
+    float xDiff = offset.x - self.initialOffset.x;
+    float yDiff = offset.y - self.initialOffset.y;
+    
+    if (ABS(xDiff) > ABS(yDiff)) {
+        // horizontal scroll
+        if (xDiff >= 0) {
+            return ScrollRight;
+        } else {
+            return ScrollLeft;
+        }
+    } else {
+        // vertical scroll
+        if (yDiff >= 0) {
+            return ScrollDown;
+        } else {
+            return ScrollUp;
+        }
+    }
 }
 
 @end
